4d4cfbc662ac93d0c479ec361652635422806520
Sébastien Counen <sebastien@LAPTOP-KN5RCJJ2.localdomain>
2019-11-22 15:18:59 +0100

    Structure

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct Medecin Medecin;
+typedef struct RendezVous RendezVous;
+
+struct Medecin
+{
+    //
+};
+
+struct RendezVous
+{
+    //
+};
 
 int main()
 {
